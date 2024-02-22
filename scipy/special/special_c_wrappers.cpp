6df1c96aa463615a72a364380b5a03ce9cb24a48
Albert Steppi <albert.steppi@gmail.com>
2024-02-22 12:00:19 -0500

    Add C wrappers for C++ special functions

--- scipy/special/special_c_wrappers.cpp
+++ scipy/special/special_c_wrappers.cpp
@@ -4,9 +4,20 @@ extern "C" {
 }
 
 #include "special/hyp2f1.h"
+#include "special/cephes/airy.h"
+#include "special/cephes/jv.h"
 
 extern "C" npy_cdouble hyp2f1_complex_wrap(double a, double b, double c, npy_cdouble zp) {
     std::complex<double> z(npy_creal(zp), npy_cimag(zp));
     std::complex<double> w = special::hyp2f1(a, b, c, z);
     return npy_cpack(real(w), imag(w));
+}
+
+
+extern "C" double cephes_airy_wrap(double x, double *ai, double *aip, double *bi, double *bip) {
+    return special::cephes::airy(x, ai, aip, bi, bip);
+}
+
+extern "C" double cephes_jv_wrap(double v, double x) {
+        return special::cephes::jv(v, x);
 }
--- scipy/special/special_c_wrappers.h
+++ scipy/special/special_c_wrappers.h
@@ -8,6 +8,8 @@ extern "C" {
 #include <numpy/npy_math.h>
 
     npy_cdouble hyp2f1_complex_wrap(double a, double b, double c, npy_cdouble zp);
+    double cephes_jv_wrap(double v, double x);
+    double cephes_airy_wrap(double x, double *ai, double *aip, double *bi, double *bip);
 
 #ifdef __cplusplus
 }
