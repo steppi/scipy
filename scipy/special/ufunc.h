b96f918abd27010c9955e63d85aa7d9d09ce7120
izaid <hi@irwinzaid.com>
2024-03-17 12:49:13 +0000

    Testing with sf_error_check_fpe

--- scipy/special/ufunc.h
+++ scipy/special/ufunc.h
@@ -7,6 +7,28 @@
 #include <numpy/npy_3kcompat.h>
 #include <numpy/ufuncobject.h>
 
+#define UFUNC_FPE_DIVIDEBYZERO 1
+#define UFUNC_FPE_OVERFLOW 2
+#define UFUNC_FPE_UNDERFLOW 4
+#define UFUNC_FPE_INVALID 8
+
+void sf_error_check_fpe(const char *func_name) {
+    int status;
+    status = PyUFunc_getfperr();
+    if (status & UFUNC_FPE_DIVIDEBYZERO) {
+        special::set_error(func_name, SF_ERROR_SINGULAR, "floating point division by zero");
+    }
+    if (status & UFUNC_FPE_UNDERFLOW) {
+        special::set_error(func_name, SF_ERROR_UNDERFLOW, "floating point underflow");
+    }
+    if (status & UFUNC_FPE_OVERFLOW) {
+        special::set_error(func_name, SF_ERROR_OVERFLOW, "floating point overflow");
+    }
+    if (status & UFUNC_FPE_INVALID) {
+        special::set_error(func_name, SF_ERROR_DOMAIN, "floating point invalid value");
+    }
+}
+
 // Just initializes everything needed, can also go in a common header
 bool SpecFun_Initialize() {
     Py_Initialize();
@@ -64,6 +86,7 @@ struct ufunc_traits<F> {
             args[0] += steps[0];
             args[1] += steps[1];
         }
+        sf_error_check_fpe("test");
     }
 };
 
