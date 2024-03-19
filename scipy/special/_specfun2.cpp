87e3f63abd74dc56ed8c4a559feff6d8fb7d4a3d
izaid <hi@irwinzaid.com>
2024-03-19 17:23:40 +0000

    Added use_math_defines in c_args

--- scipy/special/_specfun2.cpp
+++ scipy/special/_specfun2.cpp
@@ -10,7 +10,9 @@ extern "C" {
 #include "special/trig.h"
 #include "ufunc.h"
 
-// This is needed by sf_error
+// This is needed by sf_error, it is defined in "_ufuncs_extra_code_common.pxi" for "_generate_pyx.py".
+// It claims to exist to call "call PyUFunc_getfperr in a context where PyUFunc_API array is initialized", but we are
+// already in such a context
 extern "C" int wrap_PyUFunc_getfperr() { return PyUFunc_getfperr(); }
 
 extern const char *_cosine_cdf_doc;
