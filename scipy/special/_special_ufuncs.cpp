86dca97ef502030491a976f3430cc8763fd58dd8
izaid <hi@irwinzaid.com>
2024-03-25 11:35:00 +0000

    Updated specfun.py

--- scipy/special/_special_ufuncs.cpp
+++ scipy/special/_special_ufuncs.cpp
@@ -17,6 +17,8 @@
 
 using namespace std;
 
+using lpn_fff_t = void (*)(float, mdspan<float, dextents<int, 1>, layout_stride>,
+                           mdspan<float, dextents<int, 1>, layout_stride>);
 using lpn_ddd_t = void (*)(double, mdspan<double, dextents<int, 1>, layout_stride>,
                            mdspan<double, dextents<int, 1>, layout_stride>);
 using lpn_DDD_t = void (*)(complex<double>, mdspan<complex<double>, dextents<int, 1>, layout_stride>,
@@ -168,8 +170,9 @@ PyMODINIT_FUNC PyInit__special_ufuncs() {
     PyObject *kerp = SpecFun_NewUFunc({special::kerp<float>, special::kerp<double>}, "kerp", kerp_doc);
     PyModule_AddObjectRef(_special_ufuncs, "kerp", kerp);
 
-    PyObject *_lpn = SpecFun_NewGUFunc({static_cast<lpn_ddd_t>(special::lpn), static_cast<lpn_DDD_t>(special::lpn)}, 2,
-                                       "_lpn", lpn_doc, "()->(np1),(np1)");
+    PyObject *_lpn = SpecFun_NewGUFunc({static_cast<lpn_fff_t>(special::lpn), static_cast<lpn_ddd_t>(special::lpn),
+                                        static_cast<lpn_DDD_t>(special::lpn)},
+                                       2, "_lpn", lpn_doc, "()->(np1),(np1)");
     PyModule_AddObjectRef(_special_ufuncs, "_lpn", _lpn);
 
     PyObject *_lpmn =
