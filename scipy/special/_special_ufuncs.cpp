bd3ed9d64226b6311c74a4285078c75f5ddd17a5
izaid <hi@irwinzaid.com>
2024-03-25 16:50:45 +0000

    Full translation of lpmn

--- scipy/special/_special_ufuncs.cpp
+++ scipy/special/_special_ufuncs.cpp
@@ -27,6 +27,8 @@ using lpn_DDD_t = void (*)(complex<double>, mdspan<complex<double>, dextents<int
                            mdspan<complex<double>, dextents<int, 1>, layout_stride>);
 extern const char *lpn_doc;
 
+using lpmn_fff_t = void (*)(float, mdspan<float, dextents<int, 2>, layout_stride>,
+                            mdspan<float, dextents<int, 2>, layout_stride>);
 using lpmn_ddd_t = void (*)(double, mdspan<double, dextents<int, 2>, layout_stride>,
                             mdspan<double, dextents<int, 2>, layout_stride>);
 extern const char *lpmn_doc;
@@ -180,7 +182,8 @@ PyMODINIT_FUNC PyInit__special_ufuncs() {
     PyModule_AddObjectRef(_special_ufuncs, "_lpn", _lpn);
 
     PyObject *_lpmn =
-        SpecFun_NewGUFunc({static_cast<lpmn_ddd_t>(special::lpmn)}, 2, "_lpmn", lpmn_doc, "()->(mp1,np1),(mp1,np1)");
+        SpecFun_NewGUFunc({static_cast<lpmn_fff_t>(special::lpmn), static_cast<lpmn_ddd_t>(special::lpmn)}, 2, "_lpmn",
+                          lpmn_doc, "()->(mp1,np1),(mp1,np1)");
     PyModule_AddObjectRef(_special_ufuncs, "_lpmn", _lpmn);
 
     PyObject *_clpmn =
--- scipy/special/special/specfun/specfun.h
+++ scipy/special/special/specfun/specfun.h
@@ -84,9 +84,6 @@ std::complex<double> cerror(std::complex<double>);
 std::complex<double> cchg(double, double, std::complex<double>);
 std::complex<double> cgama(std::complex<double>, int);
 
-void clpmn(std::complex<double>, int, int, int, std::complex<double> *, std::complex<double> *);
-void clpn(int, std::complex<double>, std::complex<double> *, std::complex<double> *);
-
 void cyzo(int, int, int, std::complex<double>*, std::complex<double> *);
 
 void cbk(int, int, double, double, double, double *, double *);
@@ -1739,43 +1736,6 @@ inline double chgus(double x, double a, double b, int *id) {
     if (hmin != 0.0) { d2 = log10(hmin); }
     *id = 15 - (d1 - d2 < 0 ? d2 - d1 : d1 - d2);
     return hu;
-}
-
-
-inline void clpn(int n, std::complex<double> z, std::complex<double> *cpn, std::complex<double> *cpd) {
-
-    // ==================================================
-    // Purpose: Compute Legendre polynomials Pn(z) and
-    //          their derivatives Pn'(z) for a complex
-    //          argument
-    // Input :  x --- Real part of z
-    //          y --- Imaginary part of z
-    //          n --- Degree of Pn(z), n = 0,1,2,...
-    // Output:  CPN(n) --- Pn(z)
-    //          CPD(n) --- Pn'(z)
-    // ==================================================
-
-    int k;
-    std::complex<double> cp0, cp1, cpf;
-
-    cpn[0] = 1.0;
-    cpn[1] = z;
-    cpd[0] = 0.0;
-    cpd[1] = 1.0;
-    cp0 = 1.0;
-    cp1 = z;
-    for (k = 2; k <= n; k++) {
-        cpf = (2.0 * k -1.0) / k * z * cp1 - (k - 1.0) / k * cp0;
-        cpn[k] = cpf;
-        if (z == 1.0) {
-            cpd[k] = 0.5 * pow(z.real() , k+1) * k * (k + 1.0);
-        } else {
-            cpd[k] = static_cast<double>(k) * (cp1 - z * cpf) / (1.0 - z * z);
-        }
-        cp0 = cp1;
-        cp1 = cpf;
-    }
-    return;
 }
 
 
@@ -5661,41 +5621,6 @@ inline void lamv(double v, double x, double *vm, double *vl, double *dl) {
 }
 
 
-inline void lpmn(int m, int n, double x, double *pm, double *pd) {
-
-    // =====================================================
-    // Purpose: Compute the associated Legendre functions
-    //          Pmn(x) and their derivatives Pmn'(x) for
-    //          real argument
-    // Input :  x  --- Argument of Pmn(x)
-    //          m  --- Order of Pmn(x),  m = 0,1,2,...,n
-    //          n  --- Degree of Pmn(x), n = 0,1,2,...,N
-    //          mm --- Physical dimension of PM and PD
-    // Output:  PM(m,n) --- Pmn(x)
-    //          PD(m,n) --- Pmn'(x)
-    // =====================================================
-
-    int i, ls;
-    double xq;
-
-    ls = (fabs(x) > 1.0 ? -1 : 1);
-    xq = sqrt(ls * (1.0 - x * x));
-    // Ensure connection to the complex-valued function for |x| > 1
-    if (x < -1.0) {
-        xq = -xq;
-    }
-
-    /* 30 */
-    for (i = 1; i <= m; ++i) {
-        pm[i * (n + 2)] = -ls * (2.0 * i - 1.0) * xq * pm[(i - 1)*(n + 2)];
-    }
-    /* 35 */
-    for (i = 0; i <= (m > (n-1) ? n - 1: m); i++) {
-        pm[i * (n + 2) + 1] = (2.0*i+1.0)*x*pm[i * (n + 2)];
-    }
-}
-
-
 inline void lpmns(int m, int n, double x, double* pm, double* pd) {
 
     // ========================================================
@@ -5760,40 +5685,6 @@ inline void lpmns(int m, int n, double x, double* pm, double* pd) {
     for (k = 1; k <= n; k++) {
         pm[k] *= coef;
         pd[k] *= coef;
-    }
-    return;
-}
-
-
-inline void lpn(int n, double x, double *pn, double *pd) {
-
-    // ===============================================
-    // Purpose: Compute Legendre polynomials Pn(x)
-    //          and their derivatives Pn'(x)
-    // Input :  x --- Argument of Pn(x)
-    //          n --- Degree of Pn(x) ( n = 0,1,...)
-    // Output:  PN(n) --- Pn(x)
-    //          PD(n) --- Pn'(x)
-    // ===============================================
-
-    int k;
-    double p0, p1, pf;
-    pn[0] = 1.0;
-    pn[1] = x;
-    pd[0] = 0.0;
-    pd[1] = 1.0;
-    p0 = 1.0;
-    p1 = x;
-    for (k = 2; k <= n; k++) {
-        pf = (2.0*k - 1.0)/k*x*p1 - (k - 1.0)/k*p0;
-        pn[k] = pf;
-        if (fabs(x) == 1.0) {
-            pd[k] = 0.5*pow(x, k+1)*k*(k+1);
-        } else {
-            pd[k] = k*(p1 - x*pf)/(1.0 - x*x);
-        }
-        p0 = p1;
-        p1 = pf;
     }
     return;
 }
