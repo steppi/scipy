ee271b1d08c4daf2829a52727f87077da6698e02
Albert Steppi <albert.steppi@gmail.com>
2024-08-22 08:41:08 -0400

    Fix decreasing to increasing

--- scipy/special/xsf/cdflib.h
+++ scipy/special/xsf/cdflib.h
@@ -2093,7 +2093,7 @@ XSF_HOST_DEVICE inline double gdtrib(double a, double p, double x) {
     };
     double lower_bound = std::numeric_limits<double>::min();
     double upper_bound = std::numeric_limits<double>::max();
-    auto [x_left, x_right, bracket_status] = detail::bracket_root(func, 1.0, 10.0, lower_bound, upper_bound, 10.0, false);
+    auto [x_left, x_right, bracket_status] = detail::bracket_root(func, 1.0, 10.0, lower_bound, upper_bound, 10.0, true);
     if (bracket_status == 1) {
 	set_error("gdtrib", SF_ERROR_OTHER, "Answer appears to be lower than lowest search bound %g", lower_bound);
 	return lower_bound;
